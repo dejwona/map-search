f6f67aa501a0305ef8a1ca35125d421f11f716ef
dejwona <iwograf@gmail.com>
2017-02-08 21:40:48 +0100

    autocomplete

--- js/app.js
+++ js/app.js
@@ -21,6 +21,11 @@ $(function() {
   var itemHTML;
 
 
+  $('#status').fadeOut();
+  $('#preloader').delay(350).fadeOut('slow');
+  $('body').delay(350).css({'overflow':'visible'});
+
+
     /* Insert Movies to DOM input*/
   function loadCountries(country) {
     var api = 'https://restcountries.eu/rest/v1/name/';
@@ -79,7 +84,7 @@ $(function() {
 
     });
 
-    land.on('click', function(event){
+    land.on('click touchstart', function(event){
       $error.css('display', 'none');
       $get_title.attr('value', 'Country');
       loadCountriesCode(this.id);
@@ -87,6 +92,7 @@ $(function() {
       $('.land').each(function(){
         // color of map
         $(this).css({fill:'#888f94'});
+        $titleInput.val('');
 
       });
       $(this).css({fill:'rgb(161, 7, 7)'}); // red
@@ -95,6 +101,52 @@ $(function() {
   }
 
   landLand();
+
+
+  var names = [];
+
+  $('.land').each(function(i){
+    var title = $(this).attr('title');
+    names.push(title);
+  });
+
+ // autocomplete
+  var name = '';
+  $('#input').keyup(function(e) {
+    var val = $(this).val();
+
+    
+    if(val == '') {
+      $('#complete').text('');
+      return;
+    }
+
+    if (e.which === 37 || e.which === 13) {
+      e.preventDefault();
+      $('#input').val(name);
+      $('#complete').text('');
+      return;
+    }
+    
+    var find = false;
+    for (var i = 0; i < names.length; i++) {
+      name = names[i];
+      if(name.indexOf(val) === 0) {
+        find = true;
+        break;
+      } else {
+        name = '';
+      }
+    }
+    
+    if(find === true) {
+      $('#complete').text(name);
+    } else {
+      $('#complete').text('');
+    }
+  })
+
+
 
   $get_title.on('change', function(e){
     land.css({fill:'rgb(136, 143, 148)'});
@@ -110,16 +162,16 @@ $(function() {
 
   function animateInfo() {
     if ($info.css('left') === "-300px") {
-        $info.animate({ "left": "+=300px" }, "slow" );
+        $info.animate({ "left": "0px" }, "slow" );
     } else {
-        $info.attr('left','300px');
+        $info.attr('left','0px');
     } 
   }
 
 
 // animation
 $close.on('click', function() {
-  $info.animate({ "left": "-=300px" }, "slow" );
+  $info.animate({ "left": "-300px" }, "slow" );
 })
 
 
@@ -130,11 +182,8 @@ $close.on('click', function() {
 
       var title = $(this).attr('title');
       name = $titleInput.val();
-      //console.log(title);
       if ( title.toLowerCase().trim() == name.toLowerCase().trim() ) {
-        //console.log(name);
           $(this).css({fill:'rgb(161, 7, 7)'}); // red
-          console.log($(this));
       } else {
           $(this).css({fill:'rgb(136, 143, 148)'});
       }
@@ -160,8 +209,6 @@ $close.on('click', function() {
               console.log(response);
                 loadCountries(response[0].name);
                 colorMap();
-
-                //$titleInput.val('');
             }).fail(function(error) {
                 $error.css('display','block');
                 $titleInput.val('');
@@ -169,6 +216,7 @@ $close.on('click', function() {
         });
   }
   sendData();
+
 
 // zoom
 var panZoomInstance = svgPanZoom('#map', {
@@ -180,9 +228,9 @@ var panZoomInstance = svgPanZoom('#map', {
   panZoomInstance.zoom(1)
 
   $(window).resize(function(){
-          panZoomInstance.resize();
-          panZoomInstance.fit();
-          panZoomInstance.center();
+      panZoomInstance.resize();
+      panZoomInstance.fit();
+      panZoomInstance.center();
   })
 
 
@@ -198,6 +246,31 @@ $home.on('click', function(e){
   panZoomInstance.resetZoom();
 });
 
+var options = {
+  zoomEnabled: true
+, controlIconsEnabled: true
+, customEventsHandler: {
+    // Halt all touch events
+    haltEventListeners: ['touchstart', 'touchend', 'touchmove', 'touchleave', 'touchcancel']
+
+    // Init custom events handler
+  , init: function(options) {
+      // Init Hammer
+      this.hammer = Hammer(options.svgElement)
+
+      // Handle double tap
+      this.hammer.on('doubletap', function(ev){
+        options.instance.zoomIn()
+      })
+    }
+
+    // Destroy custom events handler
+  , destroy: function(){
+      this.hammer.destroy()
+    }
+  }
+}
+svgPanZoom('#map', options);
 
 
 });
